4a6121f0cd45d6324de9a34071476dbaec9f059e
Hakim ullah web developer <hakimullahdeveloper123@gmail.com>
2025-07-17 16:55:41 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+ alert("Hi Welcome to my Portfolio                                                                  Does my Portfolio provide all the information that are required in a Website ?                                                                                        Does this answer your question ?")
+// console.log();
+
+
+
+
+
+const button = document.getElementById('signupbutton');
+ button.addEventListener('click', function(){
+     alert('Hi I am a Web Developer');
+ });
+
+
+// // let Amazonfounder = "Jeffbezos" 
+// // let Who_is_the_user_of_this_computer = "Hakim Ullah "
+// // let our_teacher = "Tatheer Hussain"
+// // let what_is_my_name = "your name is Hakimullah"
+// // let who_is_my_best_friend = "your best friend is Saddam Hussain"
